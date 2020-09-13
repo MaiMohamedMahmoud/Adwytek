9f925f6301e11303f79a16237168bb10bdaa8e50
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2020-09-13 23:36:12 +0200

    register using github

--- app/src/main/java/com/marscode/pwn/adwytek/Screens/SingUp/RegisterFragment.java
+++ app/src/main/java/com/marscode/pwn/adwytek/Screens/SingUp/RegisterFragment.java
@@ -9,13 +9,21 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.marscode.pwn.adwytek.R;
 import com.marscode.pwn.adwytek.Screens.MedicineList.MedicineListActivity;
 import com.marscode.pwn.adwytek.Screens.SignIn.LoginActivity;
@@ -31,11 +39,23 @@ public class RegisterFragment extends Fragment implements RegisterInterface.Regi
     View phoneView;
     Button deletePhone;
     TextView txtSignIn;
+    EditText editTxtEmail;
+    EditText editTxtPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 
     @VisibleForTesting
@@ -54,6 +74,9 @@ public class RegisterFragment extends Fragment implements RegisterInterface.Regi
         registerPresenter = new RegisterPresenter(this);
         Button addPhone = view.findViewById(R.id.add_button);
         Button signUp = view.findViewById(R.id.btn_sign_up);
+        editTxtEmail = view.findViewById(R.id.edit_txt_email);
+        editTxtPassword = view.findViewById(R.id.edit_txt_password);
+
         addPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,12 +92,62 @@ public class RegisterFragment extends Fragment implements RegisterInterface.Regi
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                medicineListActivity();
+                registerNewUser(editTxtEmail.getText().toString(), editTxtPassword.getText().toString());
+
             }
         });
         return view;
     }
 
+    private void registerNewUser(String email, String password) {
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Please enter email!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Please enter password!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity().getApplicationContext(),
+                                    "Registration successful!",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+
+
+                            // if the user created intent to login activity
+                            medicineListActivity();
+                        } else {
+                            // Registration failed
+                            Toast.makeText(
+                                    getActivity().getApplicationContext(),
+                                    "Registration failed!!"
+                                            + " Please try again later",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+
+                        }
+                    }
+                });
+    }
 
     public void AddPhone(View view) {
         phoneView = registerPresenter.addPhone(view, phoneContainer);
