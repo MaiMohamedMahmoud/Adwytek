68eac345afe1a910735a1ef8d93f78face527bc3
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2020-10-17 22:43:59 +0200

    fixing errors in new medicine page

--- app/src/main/java/com/marscode/pwn/adwytek/Screens/NewMedicine/NewMedicineFragment.java
+++ app/src/main/java/com/marscode/pwn/adwytek/Screens/NewMedicine/NewMedicineFragment.java
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +20,10 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -40,7 +45,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class NewMedicineFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener, View.OnFocusChangeListener {
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
+
+public class NewMedicineFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener, TextWatcher {
     LinearLayout medication_dose_data;
     View doseView;
     NewMedicinePresenter newMedicinePresenter;
@@ -48,6 +55,9 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
     TextView txt_dose_time;
     EditText txt_dose_quantity;
     EditText edit_txt_medicine_name;
+    Calendar currentDate;
+    TextInputLayout dose_txt_input_layout;
+    TextInputLayout medicine_name_txt_input_layout;
     Button start_btn;
     Button end_btn;
     Button btn_mon, btn_tue, btn_wen, btn_thu, btn_fri, btn_sat, btn_sun;
@@ -62,6 +72,7 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
     Button addMedicineBtn;
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
+    AwesomeValidation mAwesomeValidation;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -73,12 +84,14 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_medicine, container, false);
+        mAwesomeValidation = new AwesomeValidation(BASIC);
         mDatabase = FirebaseDatabase.getInstance().getReference();
         medication_dose_data = view.findViewById(R.id.dose_container);
         start_btn = view.findViewById(R.id.btn_start_date);
         end_btn = view.findViewById(R.id.btn_end_date);
         addMedicineBtn = view.findViewById(R.id.save);
         edit_txt_medicine_name = view.findViewById(R.id.edit_txt_medicine_name);
+        medicine_name_txt_input_layout = view.findViewById(R.id.medicine_name_text_input_layout);
         daysList = new ArrayList<>();
         doseList = new ArrayList<>();
         btn_mon = view.findViewById(R.id.btn_mon);
@@ -102,7 +115,10 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
         // Apply the adapter to the spinner
         spinner_frequency_time.setAdapter(adapter_frequency_time);
         spinner_frequency_time.setOnItemSelectedListener(this);
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        getCurrentDate();
+        start_btn.setText(sdf.format(currentDate.getTime()));
+        end_btn.setText(sdf.format(currentDate.getTime()));
         start_btn.setOnClickListener(this);
         end_btn.setOnClickListener(this);
         btn_mon.setOnClickListener(this);
@@ -119,8 +135,10 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
-        Log.i("yaraaaaab yaraaab", adapterView.getItemAtPosition(pos) + " " + l);
         medication_dose_data.removeAllViewsInLayout();
+        doseQuantityList.clear();
+        doseTimeList.clear();
+        doseList.clear();
         for (int i = 0; i <= l; i++) {
             frequency_of_intake = l;
             addDosePerTime(view);
@@ -134,9 +152,11 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
         for (int i = 0; i <= childCount; i++) {
             txt_dose_time = doseView.findViewById(R.id.dose_time);
             txt_dose_quantity = doseView.findViewById(R.id.dose_quantity);
+            dose_txt_input_layout = doseView.findViewById(R.id.dose_quantity_text_input_layout);
             txt_dose_time.setOnClickListener(this);
-            txt_dose_quantity.setOnFocusChangeListener(this);
         }
+        dose_txt_input_layout.getEditText().addTextChangedListener(this);
+
     }
 
     @Override
@@ -149,7 +169,8 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
         mYear = cal.get(Calendar.YEAR);
         mMonth = cal.get(Calendar.MONTH);
         mDay = cal.get(Calendar.DAY_OF_MONTH);
-
+        cal.set(mYear, mMonth, mDay);
+        currentDate = cal;
     }
 
     public void getCurrentTime() {
@@ -167,18 +188,32 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
                         final Calendar calendar = Calendar.getInstance();
                         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
                         calendar.set(year, monthOfYear, dayOfMonth);
-                        date_picker = calendar.getTime();
-                        Log.i("yarab ", "inside" + date_picker);
-                        String dateString = sdf.format(date_picker);
-                        buttonView.setText(dateString);
-                        if (buttonView == start_btn) {
-                            startDate = date_picker;
-                        }
-                        if (buttonView == end_btn) {
-                            endDate = date_picker;
+                        if (calendar.before(currentDate)) {
+                            Toast.makeText(getContext(), getString(R.string.error_date), Toast.LENGTH_LONG).show();
+                        } else {
+                            date_picker = calendar.getTime();
+                            Log.i("yarab ", "inside" + date_picker);
+                            String dateString = sdf.format(date_picker);
+                            buttonView.setText(dateString);
+                            if (buttonView == start_btn) {
+                                startDate = date_picker;
+                                if (endDate != null) {
+                                    if (endDate.before(startDate)) {
+                                        endDate = startDate;
+                                        end_btn.setText(sdf.format(endDate));
+                                    }
+                                }
+                            }
+                            if (buttonView == end_btn) {
+                                endDate = date_picker;
+                            }
                         }
                     }
                 }, mYear, mMonth, mDay);
+        datePickerDialog.getDatePicker().setMinDate(currentDate.getTime().getTime());
+        if (startDate != null && buttonView == end_btn) {
+            datePickerDialog.getDatePicker().setMinDate(startDate.getTime());
+        }
         datePickerDialog.show();
     }
 
@@ -187,11 +222,20 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
                 new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        String dateString = hourOfDay + ":" + minute;
-                        txt.setText(dateString);
+                        if (txt.getText().toString() == getText(R.string.time)) {
+                            String dateString = hourOfDay + ":" + minute;
+                            txt.setText(dateString);
 
-                        doseTimeList.add(dateString);
-                        Log.i("yarab", dateString.toString() + "time pich" + dateString);
+                            doseTimeList.add(dateString);
+                            Log.i("yarab", dateString.toString() + "time pich" + dateString);
+                        } else {
+                            doseTimeList.remove(txt.getText().toString());
+                            String dateString = hourOfDay + ":" + minute;
+                            txt.setText(dateString);
+                            doseTimeList.add(dateString);
+                            Log.i("yarab", dateString.toString() + "time pich" + dateString);
+
+                        }
                     }
                 }, mHour, mMinute, true);
         timePickerDialog.show();
@@ -199,29 +243,20 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
 
     public void addNewMedicine() {
 
-
-     /*   Date c = Calendar.getInstance().getTime();
-        System.out.println("Current time => " + c);
-
-        Dose dose1 = new Dose("Su", c, "2");
-        Dose dose2 = new Dose("Tu", c, "3");
-*/
-
         for (int days = 0; days < daysList.size(); days++) {
             for (int dose = 0; dose <= frequency_of_intake; dose++) {
-                Log.i("yarab inside loop", frequency_of_intake + "dose time" + doseTimeList.get(dose) + doseQuantityList.get(dose));
                 Dose doseObj = new Dose(daysList.get(days),
                         doseTimeList.get(dose),
                         doseQuantityList.get(dose));
                 doseList.add(doseObj);
             }
         }
-        /*doseList.add(dose1);
-        doseList.add(dose2);*/
 
+        if (endDate.before(startDate)) {
+            endDate = startDate;
+        }
         Medicine medicineOmega = new Medicine(edit_txt_medicine_name.getText().toString(), startDate, endDate, frequency_of_intake + 1 + "", doseList);
 
-        Log.i("yarab", edit_txt_medicine_name.getText().toString() + "start " + startDate + "End " + endDate + "Freq " + frequency_of_intake + "dose " + doseList.size());
         newMedicinePresenter.addNewMedicine(medicineOmega);
     }
 
@@ -234,80 +269,88 @@ public class NewMedicineFragment extends Fragment implements AdapterView.OnItemS
                 getCurrentDate();
                 //Launch Date Picker Dialog
                 showDatePickerDialog(start_btn);
-                Log.i("yarab ", startDate + "date picker " + date_picker);
                 break;
             case R.id.btn_end_date:
                 // Get Current Date
                 getCurrentDate();
                 //Launch Date Picker Dialog
                 showDatePickerDialog(end_btn);
-                Log.i("yarab ", endDate + "date picker " + date_picker);
                 break;
 
             case R.id.btn_mon:
                 btn_mon.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Mo");
+                btn_mon.setEnabled(false);
                 break;
             case R.id.btn_tue:
                 btn_tue.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
                 ;
                 daysList.add("Tue");
+                btn_tue.setEnabled(false);
                 break;
             case R.id.btn_wen:
                 btn_wen.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Wen");
+                btn_wen.setEnabled(false);
                 break;
             case R.id.btn_thu:
                 btn_thu.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Thu");
+                btn_thu.setEnabled(false);
                 break;
             case R.id.btn_fri:
                 btn_fri.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Fr");
+                btn_fri.setEnabled(false);
                 break;
             case R.id.btn_sat:
                 btn_sat.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Sa");
+                btn_sat.setEnabled(false);
                 break;
             case R.id.btn_sun:
                 btn_sun.setBackgroundTintList(getContext().getResources().getColorStateList(R.color.colorxml));
-                ;
                 daysList.add("Su");
+                btn_sun.setEnabled(false);
                 break;
 
             case R.id.save:
-                addNewMedicine();
+
+                boolean valid = validate();
+                if (valid) {
+                    addNewMedicine();
+                }
                 break;
             default:
                 // Get Current Time
                 getCurrentTime();
                 // Launch Time Picker Dialog
-                Log.i("TextView", view.getId() + "");
                 showTimePickerDialog((TextView) view);
         }
 
 
     }
 
+    private boolean validate() {
+        mAwesomeValidation.addValidation(getActivity(), R.id.medicine_name_text_input_layout, "[a-zA-Z\\s]+", R.string.medicine_required);
+        mAwesomeValidation.addValidation(getActivity(),R.id.dose_quantity_text_input_layout,"[0-9]",R.string.quanity_required);
+        mAwesomeValidation.addValidation(getActivity(),R.id.dose_time,"[0-9]",R.string.medicine_required);
+        return mAwesomeValidation.validate();
+    }
+
     @Override
-    public void onFocusChange(View view, boolean hasFocus) {
-        String input;
-        EditText editText;
-        if (!hasFocus) {
-            editText = (EditText) view;
-            input = editText.getText().toString();
-            doseQuantityList.add(input);
-        } else {
-            editText = (EditText) view;
-            input = editText.getText().toString();
-            if (doseQuantityList.contains(input)) {
-                doseQuantityList.remove(input);
-            }
+    public void beforeTextChanged(CharSequence text, int start, int count, int after) {
+        if (doseQuantityList.contains(text.toString())) {
+            doseQuantityList.remove(text.toString());
         }
+    }
+
+    @Override
+    public void onTextChanged(CharSequence text, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        doseQuantityList.add(editable.toString());
     }
 }
